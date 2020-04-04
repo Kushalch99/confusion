de39eafaf6b471a14e7f3d9c2421eed1c614c64b
Kushal Choudhary <kushalch99@gmail.com>
2020-04-04 15:54:41 +0530

    components part1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import MenuComponent from './components/MenuComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component{
+  render(){
+    return (
+    <div >
+      <Navbar dark color="secondary">
+        <div className="container">
+          <NavbarBrand href="/">Ristorante Con fusion</NavbarBrand>
+        </div>
+      </Navbar>
+      <MenuComponent />
     </div>
   );
+  }
 }
 
 export default App;
